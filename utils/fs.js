9e96a65182ecaaf08e467e98d48f61a85b9b9928
Fenzland <Fenzland@github.com>
2019-09-01 00:51:28 +0800

    include fs util

---
+++ utils/fs.js
@@ -0,0 +1,29 @@
+import { decode, } from './text-encoder.js';
+export { exists as file_exists, } from 'https://deno.land/std@v0.12/fs/exists.ts';
+
+/**
+ * Check whether a file is a file (not directory or symbol link).
+ * 
+ * @param path (string)
+ * 
+ * @return ~(boolean)
+ */
+export async function is_file( path, )
+{
+	return (await Deno.stat( path, )).isFile();
+}
+
+export async function file_length( path, )
+{
+	return (await Deno.stat( path, )).len;
+}
+
+export async function read_file( path, { asText=true, }={}, )
+{
+	const $array= await Deno.readFile( path, );
+	
+	if( asText )
+		return decode( $array, );
+	else
+		return $array;
+}

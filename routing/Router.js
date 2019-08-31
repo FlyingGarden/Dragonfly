36bcc5cbf410632231a7cac0019d994c688fcd16
Fenzland <Fenzland@github.com>
2019-08-31 23:35:44 +0800

    Router support default controllerPath by tracing back

--- routing/Router.js
+++ routing/Router.js
@@ -1,3 +1,4 @@
+import * as Path from '../utils/path.js';
 
 export default class Router
 {
@@ -15,12 +16,24 @@ export default class Router
 	 * Consturct a router
 	 * 
 	 * @param 0.routes          {}[]
-	 * @param 0.controllerPath (string)
+	 * @param 0.?controllerPath (string)
 	 */
-	constructor( { routes, controllerPath, }, )
+	constructor( { routes, controllerPath=defaultControllerPath(), }, )
 	{
 		this.#controllerPath=controllerPath;
 		
 		this.#routes= routes;
 	}
+}
+
+/**
+ * Generate the default web root
+ * 
+ * @return (string)
+ */
+function defaultControllerPath()
+{
+	const file= Path.traceBack( 2, );
+	
+	return `${Path.dirname( file, )}/controllers`;
 }

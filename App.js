be9ec35bfa51a072c9ae77c2e63545b684633747
Fenzland <Fenzland@github.com>
2019-08-31 23:05:14 +0800

    App constructed with router and webRoot

--- App.js
+++ App.js
@@ -4,5 +4,25 @@
  */
 export default class App
 {
+	/**
+	 * @type {Router}
+	 */
+	#router;
 	
+	/**
+	 * @type (string)
+	 */
+	#webRoot;
+	
+	/**
+	 * Construct app
+	 * 
+	 * @param 0.router  {Router}
+	 * @param 0.webRoot (string)
+	 */
+	constructor( { router, webRoot, }, )
+	{
+		this.#router= router;
+		this.#webRoot= webRoot;
+	}
 }

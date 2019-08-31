dae9e4febc408b0fc41628cd10c45572f2c7d3d5
Fenzland <Fenzland@github.com>
2019-08-31 23:08:10 +0800

    App support default webRoot by tracing back

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import * as Path from './utils/path.js';
 
 /**
  * main Application class of Dragonfly
@@ -20,9 +21,21 @@ export default class App
 	 * @param 0.router  {Router}
 	 * @param 0.webRoot (string)
 	 */
-	constructor( { router, webRoot, }, )
+	constructor( { router, webRoot=defaultWebRoot(), }, )
 	{
 		this.#router= router;
 		this.#webRoot= webRoot;
 	}
+}
+
+/**
+ * Generate the default web root
+ * 
+ * @return (string)
+ */
+function defaultWebRoot()
+{
+	const file= Path.traceBack( 2, );
+	
+	return Path.dirname( file, ).replace( /^file:\/\//, '', ).replace( /\/([A-Z]:\/)/, '$1', );
 }
